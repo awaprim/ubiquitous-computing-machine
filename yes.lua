ac70e2d216f6c151dfe2e7a9e180853ee8b89ea4
awaprimPL <59965864+awaprimPL@users.noreply.github.com>
2021-01-05 17:31:20 +0100

    Update yes.lua

--- yes.lua
+++ yes.lua
@@ -1,7 +1,19 @@
-while wait(900) do
+--while wait(900) do
 link = "https://docs.google.com/forms/u/0/d/e/1FAIpQLSdg0cl2pXaf429gxqSWn6P5ZasqsTh7dJXsRYjxS7mDtJngEQ/formResponse"
 ign = game.Players.LocalPlayer.Name
 placeid = game.PlaceId
+if placeid == 2317712696 then
+    placeid = "The Wild West"
+end
+if placeid == 606849621 then
+    placeid = "Jailbreak"
+end
+if placeid == 4581966615 then
+    placeid = "Anomic"
+end
+if placeid == 3145447020 then
+   placeid = "Mount Everest Climbing Roleplay" 
+end
 pcall(function()
     local content = syn.request(
         {
@@ -12,4 +24,4 @@ pcall(function()
         }
     )
 end)
-end
+--end
